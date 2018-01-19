80f5b04a4e3df6c7ac6f3b392260213778e0e59a
Raphael Matile <raphael.matile@gmail.com>
2018-01-19 15:23:49 +0100

    Add loggers

--- src/main/java/org/provotum/backend/socket/controller/BallotController.java
+++ src/main/java/org/provotum/backend/socket/controller/BallotController.java
@@ -7,12 +7,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.logging.Logger;
+
 @Controller
 public class BallotController {
+
+    private static final Logger logger = Logger.getLogger(BallotController.class.getName());
 
     @MessageMapping("/contracts/ballot/deploy")
     @SendTo("/topic/deployments")
     public BallotDeploymentResponse deployBallot(BallotDeploymentRequest request) {
+        logger.info("Received ballot deployment request");
+
         return new BallotDeploymentResponse("success", "Deployment successful", new Contract("ballot", "0x123"));
     }
 }
--- src/main/java/org/provotum/backend/socket/controller/ZeroKnowledgeController.java
+++ src/main/java/org/provotum/backend/socket/controller/ZeroKnowledgeController.java
@@ -7,12 +7,18 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.logging.Logger;
+
 @Controller
 public class ZeroKnowledgeController {
+
+    private static final Logger logger = Logger.getLogger(ZeroKnowledgeController.class.getName());
 
     @MessageMapping("/contracts/zero-knowledge/deploy")
     @SendTo("/topic/deployments")
     public ZeroKnowledgeDeploymentResponse deployBallot(ZeroKnowledgeDeploymentRequest request) {
+        logger.info("Received zero-knowledge deployment request");
+
         return new ZeroKnowledgeDeploymentResponse("success", "Deployment successful", new Contract("zero-knowledge", "0x123"));
     }
 }
